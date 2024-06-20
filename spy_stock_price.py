f8fd20147b3d35d4bc4f962845d8a1506dea14f7
Tony Muela <166438176+tonyMuela@users.noreply.github.com>
2024-06-20 09:53:53 -0700

    Create spy_stock_price.py
    A hash map of the daily price of stock: SPY

---
+++ spy_stock_price.py
@@ -0,0 +1,17 @@
+spy_stock_price = {
+
+    'june 3': 527.8,
+    'june 4': 528.39,
+    'june 5': 534.67,
+    'june 6': 534.66,
+    'june 7': 534.01,
+    'june 10': 535.66,
+    'june 11': 536.95,
+    'june 12': 541.36,
+    'june 13': 542.45,
+    'june 14': 542.78,
+    'june 17': 547.1,
+    'june 18': 548.49,
+    'june 19': 548.8
+}
+print(spy_stock_price['june 19'])
